d544fd7f02f0a75723aaf56b8ed38cfd7fe31145
Noah <Noah.Whiffen@keyin.com>
2025-08-29 11:01:49 -0230

    added new up to date projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,22 +9,42 @@ export type Project = {
 
 export const projects: Project[] = [
   {
-    title: "Password Strength Checker",
-    img: "images/Projects/PasswordChecker.png",
-    alt: "Password Strength Checker",
+    title: "Airport API",
+    img: "images/Projects/airportapi.png",
+    alt: "Airport API",
     desc:
-      "A simple GUI to validate the strength of your password by checking a list of common passwords and other criteria.",
-    tools: ["Python", "Tkinter"],
-    href: "https://github.com/AceOfSpades98/passwordStrengthChecker",
+      "An API used to track flights and all relevant information. Populated with dummy data and demonstrated with Postman.",
+    tools: ["Java", "Maven", "MySQL"],
+    href:
+      "https://github.com/Noah-Lambe/Sprint1_AirportAPI",
   },
   {
-    title: "Pay Calculator",
-    img: "images/Projects/PayCalculator.png",
-    alt: "Pay Calculator",
+    title: "Farewell Finder",
+    img: "images/Projects/farewellfinder.png",
+    alt: "Airport API",
     desc:
-      "A simple GUI to calculate a net pay rate when an employee's rate of pay and hours worked are entered. My first ever project!",
-    tools: ["Python", "Tkinter"],
-    href: "https://github.com/AceOfSpades98/payCalculator",
+      "The front end for the airport API, complete with user authentication and the ability to search flights and filter based on different criteria.",
+    tools: ["Javascript", "React", "CSS"],
+    href:
+      "https://github.com/Noah-Lambe/Sprint1_AirportAPI",
+  },
+  {
+    title: "Binary Search Tree API",
+    img: "images/Projects/binarysearchtree.jpg",
+    alt: "Binary Search Tree API",
+    desc:
+      "An API which accepts user input and stores the values as JSON",
+    tools: ["Java", "Maven", "Springboot", "MySQL"],
+    href: "https://github.com/AceOfSpades98/FinalSprintDSA",
+  },
+    {
+    title: "BST Front End",
+    img: "images/Projects/bstfrontend.jpg",
+    alt: "BST frontend",
+    desc:
+      "A React app for the BST API that displays user input as JSON data. Also allows you to view all past trees as JSON data.",
+    tools: ["Javascript", "React", "CSS"],
+    href: "https://github.com/AceOfSpades98/FinalSprintDSA-FrontEnd",
   },
   {
     title: "Live WebSocket Chat",
@@ -32,36 +52,18 @@ export const projects: Project[] = [
     alt: "Live WebSocket Chat",
     desc:
       "A live chat application with login; stores user credentials and messages in a database.",
-    tools: ["JavaScript", "Node.js", "Express.js", "MongoDB"],
+    tools: ["Javascript", "Node.js", "Express.js", "MongoDB"],
     href:
       "https://github.com/Keyin-SD14-S3/fullstack-databases-final-sprint-team-super-awesome-team-nl-nw-js-hs",
   },
   {
-    title: "Genealogy Website",
-    img: "images/Projects/whiffenGene.png",
-    alt: "Genealogy Website",
-    desc:
-      "A simple webpage intended to be used for my genealogy research.",
-    tools: ["HTML", "CSS"],
-    href: "https://github.com/NoahWhiffen/familyTreeSite",
-  },
-  {
-    title: "Dog Image API",
+    title: "Dog Image Gallery",
     img: "images/Projects/dogApi.png",
     alt: "Dog API App",
     desc:
       "Pulls dog images from an API based on selected breed and desired number of images.",
     tools: ["JavaScript", "React", "CSS"],
     href: "https://github.com/NoahWhiffen/dog-breed-gallery",
-  },
-  {
-    title: "To-Do List App",
-    img: "images/Projects/ToDoApp.png",
-    alt: "To-Do App",
-    desc:
-      "A to-do list app that allows the user to keep track of tasks.",
-    tools: ["JavaScript", "React", "CSS"],
-    href: "https://github.com/NoahWhiffen/to-do-app",
   },
   {
     title: "Gym Management App",
@@ -78,9 +80,35 @@ export const projects: Project[] = [
     alt: "Math Competition App",
     desc:
       "An Express app that generates random math questions and tracks high scores.",
-    tools: ["JavaScript", "Express.js", "CSS"],
+    tools: ["Javascript", "Express.js", "CSS"],
     href:
       "https://github.com/Keyin-SD14-S3/fullstack-qap-2-NoahWhiffen",
+  },
+    {
+    title: "Password Strength Checker",
+    img: "images/Projects/PasswordChecker.png",
+    alt: "Password Strength Checker",
+    desc:
+      "A simple GUI to validate the strength of your password by checking a list of common passwords and other criteria.",
+    tools: ["Python", "Tkinter"],
+    href: "https://github.com/AceOfSpades98/passwordStrengthChecker",
+  },
+  {
+    title: "One Stop Insurance Company",
+    img: "images/Projects/OneStopInsurance.png",
+    alt: "Insurance Company App",
+    desc: "An application that generates invoices for an insurance company.",
+    tools: ["Python"],
+    href: "https://github.com/AceOfSpades98/qap4-Term1"
+  },
+  {
+    title: "Genealogy Website",
+    img: "images/Projects/whiffenGene.png",
+    alt: "Genealogy Website",
+    desc:
+      "A simple webpage intended to be used for my genealogy research.",
+    tools: ["HTML", "CSS"],
+    href: "https://github.com/NoahWhiffen/familyTreeSite",
   },
 ];
 
@@ -121,4 +149,31 @@ export const projects: Project[] = [
   //     "A CLI app to manage conference information (speakers, topics, etc.) using PostgreSQL queries.",
   //   tools: ["JavaScript", "Node.js", "PostgreSQL"],
   //   href: "https://github.com/NoahWhiffen/qap4-Term1/tree/main/PythonQAP4",
+  // },
+  // {
+  //   title: "To-Do List App",
+  //   img: "images/Projects/ToDoApp.png",
+  //   alt: "To-Do App",
+  //   desc:
+  //     "A to-do list app that allows the user to keep track of tasks.",
+  //   tools: ["Javascript", "React", "CSS"],
+  //   href: "https://github.com/NoahWhiffen/to-do-app",
+  // },
+  // {
+  //   title: "Pay Calculator",
+  //   img: "images/Projects/PayCalculator.png",
+  //   alt: "Pay Calculator",
+  //   desc:
+  //     "A simple GUI to calculate a net pay rate when an employee's rate of pay and hours worked are entered. My first ever project!",
+  //   tools: ["Python", "Tkinter"],
+  //   href: "https://github.com/AceOfSpades98/payCalculator",
+  // },
+  // {
+  //   title: "To-Do List App",
+  //   img: "images/Projects/ToDoApp.png",
+  //   alt: "To-Do App",
+  //   desc:
+  //     "A to-do list app that allows the user to keep track of tasks.",
+  //   tools: ["JavaScript", "React", "CSS"],
+  //   href: "https://github.com/NoahWhiffen/to-do-app",
   // },
